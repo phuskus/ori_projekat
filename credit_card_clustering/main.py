a4404dfdfdc533629385f70a19b7faf38517be3a
Stefan Stegić <stefanstegic@gmail.com>
2020-07-03 15:33:26 +0200

    Implement clustering interpretation.

--- credit_card_clustering/main.py
+++ credit_card_clustering/main.py
@@ -3,11 +3,20 @@ import numpy as np
 from sklearn.cluster import KMeans
 from yellowbrick.cluster import KElbowVisualizer
 from tabulate import tabulate
+import math
 """
 PROGRAM PARAMETERS
 """
+# Where to load the csv data from
 CSV_PATH = "credit_card_data.csv"
+
+# The range of cluster numbers to try out when
+# finding the best one
 CLUSTER_RANGE = (2, 15)
+
+# Quality levels by which to rank each column of a cluster
+# Assumed to be in ascending order
+RANKING_CATEGORIES = ["Very low", "Low", "Average", "High", "Very High"]
 
 """ 
 Load raw CSV
@@ -40,7 +49,7 @@ kmeans = KMeans(n_clusters=cluster_num)
 kmeans.fit(np_array)
 
 """
-Pretty print cluster center data
+Output group info table
 """
 headers = ["Group"]
 headers.extend(dataframe.columns.values[1:])
@@ -49,3 +58,69 @@ for i in range(len(table)):
     table[i].insert(0, i+1)
 
 print(tabulate(table, headers=headers))
+
+groupSizes = [0 for i in range(cluster_num)]
+totalSize = len(kmeans.labels_)
+for label in kmeans.labels_:
+    groupSizes[label] += 1
+
+"""
+Output data interpretation
+"""
+sortedColumns = {}
+for i in range(1, len(headers)):
+    # Sort groups by this column in ascending order
+    col = headers[i]
+    sortedColumns[col] = { "min": float("inf"), "max": float("-inf"), "values": [] }
+    for row in table:
+        if row[i] < sortedColumns[col]["min"]:
+            sortedColumns[col]["min"] = row[i]
+        if row[i] > sortedColumns[col]["max"]:
+            sortedColumns[col]["max"] = row[i]
+
+
+        if len(sortedColumns[col]) == 0:
+            sortedColumns[col]["values"].append(row[i])
+            continue
+
+        place = 0
+        while place < len(sortedColumns[col]["values"]) and sortedColumns[col]["values"][place] < row[i]:
+            place += 1
+
+        if place >= len(sortedColumns[col]):
+            sortedColumns[col]["values"].append(row[i])
+        else:
+            sortedColumns[col]["values"].insert(place, row[i])
+
+rankHeight = 1.0 / len(RANKING_CATEGORIES)
+rankings = []
+for row in table:
+    # Give each column a rank
+    myRanking = []
+    for i in range(1, len(row)):
+        diff = sortedColumns[headers[i]]["max"] - sortedColumns[headers[i]]["min"]
+        myHeight = (row[i] - sortedColumns[headers[i]]["min"]) / diff
+        index = min(len(RANKING_CATEGORIES)-1, int(myHeight / rankHeight))
+        myRanking.append(RANKING_CATEGORIES[index])
+    rankings.append(myRanking)
+
+def getLoadBar(rankName):
+    bar = ""
+    been = False
+    for rank in RANKING_CATEGORIES:
+        if rank == rankName:
+            bar += ">"
+            been = True
+        elif been:
+            bar += " "
+        else:
+            bar += "="
+    return "[%s]" % bar
+
+for i, rankList in enumerate(rankings):
+    print("\n\n-----Group %d-----" % (i+1))
+    print("Size: %d (%s%%)" % (groupSizes[i], str(round(float(groupSizes[i]) / totalSize * 100.0, 2))))
+    table = []
+    for j in range(len(rankList)):
+        table.append([ headers[j+1], rankList[j], getLoadBar(rankList[j])])
+    print(tabulate(table, tablefmt="plain"))
